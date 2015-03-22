132db5831a6a9c8ec522d23ae2a18c2856c4b3eb
Prashant Sankhla <prashant.sankhla@intimetec.com>
2015-03-22 22:30:50 +0530

    Chap-4 DONE

--- features/adopting_puppies.feature
+++ features/adopting_puppies.feature
@@ -7,17 +7,17 @@ Feature: Adopting puppies
     Given I am on the puppy adoption site
 
   Scenario: Validate cart with one puppy
-    When I click the first View Details button
+    When I click the View Details button for "Brook"
     And I click the Adopt Me button
     Then I should see "Brook" as the name for line item 1
     And I should see "$34.95" as the subtotal for line item 1
     And I should see "$34.95" as the cart total
 
   Scenario: Validate cart with two puppies
-    When I click the first View Details button
+    When I click the View Details button for "Brook"
     And I click the Adopt Me button
     And I click the Adopt Another Puppy button
-    And I click the second View Details button
+    And I click the View Details button for "Hanna"
     And I click the Adopt Me button
     Then I should see "Brook" as the name for line item 1
     And I should see "$34.95" as the subtotal for line item 1
--- features/step_definitions/adopting_puppy_steps.rb
+++ features/step_definitions/adopting_puppy_steps.rb
@@ -1,69 +1,55 @@
 Given(/^I am on the puppy adoption site$/) do
-  @browser.goto "http://puppies.herokuapp.com"
-  @home = HomePage.new(@browser)
+  visit(HomePage)
 end
 
-When(/^I click the first View Details button$/) do
-  @home.select_puppy_number 1
-  @details = DetailsPage.new(@browser)
-end
-
-When(/^I click the second View Details button$/) do
-  @home.select_puppy_number 2
-  @details = DetailsPage.new(@browser)
+When /^I click the View Details button for "([^"]*)"$/ do |name|
+  on(HomePage).select_puppy name
 end
 
 When(/^I click the Adopt Me button$/) do
-  @details.add_to_cart
-  @cart = ShoppingCartPage.new(@browser)
+  on(DetailsPage).add_to_cart
 end
 
 When(/^I click the Adopt Another Puppy button$/) do
-  @cart.continue_shopping
+  on(ShoppingCartPage).continue_shopping
 end
 
 When(/^I click the Complete the Adoption button$/) do
-  @cart.proceed_to_checkout
-  @checkout = CheckoutPage.new(@browser)
-
+  on(ShoppingCartPage).proceed_to_checkout
 end
 
 When(/^I enter "(.*?)" in the name field$/) do |name|
-  @checkout.name = name
+  on(CheckoutPage).name = name
 end
 
 When(/^I enter "(.*?)" in the address field$/) do |address|
-  @checkout.address = address
+  on(CheckoutPage).address = address
 end
 
 When(/^I enter "(.*?)" in the email field$/) do |email|
-  @checkout.email = email
+  on(CheckoutPage).email = email
 end
 
 When(/^I select "(.*?)" from the pay with dropdown$/) do |pay_type|
-  @checkout.pay_type = pay_type
+  on(CheckoutPage).pay_type = pay_type
 end
 
 When(/^I click the Place Order button$/) do
-  @checkout.place_order
+  on(CheckoutPage).place_order
 end
 
 Then(/^I should see "(.*?)"$/) do |expected|
-  #fail 'Browser text did not match expected value' unless @browser.text.include? arg1
-  expect(@browser.text).to include expected
+  expect(@current_page.text).to include expected
 end
 
-Then(/^I should see "(.*?)" as the name for line item (\d+)$/) do |name, line_item|
-#  row = row_for(line_item.to_i)
-  expect(@cart.name_for_line_item(line_item.to_i)).to include name
+Then (/^I should see "([^"]*)" as the name for (line item \d+)$/) do |name, line_item|
+  expect(on(ShoppingCartPage).name_for_line_item(line_item)).to include name
 end
 
-Then(/^I should see "(.*?)" as the subtotal for line item (\d+)$/) do |subtotal, line_item|
-  # row = row_for(line_item.to_i)
-  expect(@cart.subtotal_for_line_item(line_item.to_i)).to eql subtotal
+Then(/^I should see "([^"]*)" as the subtotal for (line item \d+)$/) do |subtotal, line_item|
+  expect(on(ShoppingCartPage).subtotal_for_line_item(line_item)).to eql subtotal
 end
 
 Then(/^I should see "(.*?)" as the cart total$/) do |total|
-  expect(@cart.cart_total).to eql total
+  expect(on(ShoppingCartPage).cart_total).to eql total
 end
-
